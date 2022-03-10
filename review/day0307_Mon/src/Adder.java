ef9b3113f40227bed109c76b0314263680e50a8e
MINSEOK PARK <seiren1005@gmail.com>
2022-03-10 20:52:48 +0900

    [UPDATE] update contents
    1. return type of method

--- review/day0307_Mon/src/Adder.java
+++ review/day0307_Mon/src/Adder.java
@@ -1,3 +1,28 @@
 public class Adder {
     
+    int result;
+    // Field(필드)
+
+    public int add(int x, int y) {
+        // 리턴 타입이 int -> int값을 리턴해 줘야됨
+        return result = x + y;
+    }
+
+    public void method() {
+        // 리턴 타입 void -> return 값 없음
+    }
+
+    public int sum(int[] arr) {
+        // 메소드의 파라미터로 배열 가능
+        int sum = 0;
+        return sum;
+    }
+
+    public int[] makeArr() {
+        return new int[10];
+        // 리턴 타입을 배열로 선언하고 배열을 리턴하는 것도 가능
+    }
+
+    
 }
+

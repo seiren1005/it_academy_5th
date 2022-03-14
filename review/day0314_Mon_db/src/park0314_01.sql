99803d718f943a9ca942e8aaaa2089a78d74afa9
ms <>
2022-03-14 20:03:34 +0900

    [UPDATE] update contents
    practice about multiple rows sub query

--- review/day0314_Mon_db/src/park0314_01.sql
+++ review/day0314_Mon_db/src/park0314_01.sql
@@ -82,3 +82,53 @@ WHERE hirdate = (
         WHERE name = '조인형'
         );
 
+
+
+-- At emp2 TABLE, 전체 직원 중 과장 직급의 최소 연봉자보다 연봉이 높은 사람의
+-- 이름, 직급, 연봉 출력
+
+-- 1) 직급 = '과장' 의 최소 연봉
+-- 2) 1) 값보다 연봉이 높은 사람 이름, 직급, 연봉 출력
+
+SELECT name, position, TO_CHAR(pay, '999,999,999') || ' 원' "SALARY"
+FROM emp2
+WHERE pay > ANY(
+    SELECT pay
+    FROM emp2
+    WHERE position = '과장'
+    );
+
+
+-- At student TABLE, 학생중 4학년 학생들의 체중에서 가장 적게 나가는 학생보다
+-- 몸무게가 적은 학생의 이름, 학년, 몸무게
+
+-- 1) 학년 = 4 이면서 체중이 가장 작은 사람
+-- 2) 1) 값보다 체중이 작은 사람의 이름, 학년, 키
+
+SELECT name, grade, weight
+FROM student
+WHERE weight < ALL(
+    SELECT weight
+    FROM student
+    WHERE grade = 4
+    );
+
+
+-- 다중 컬럼 서브 쿼리 : 결과가 여러 컬럼인 경우
+-- 여러개 컬럼의 값을 한꺼번에 비교하기 위해서 사용
+-- At student TABLE, 각 학년별로 최대 키를 가진 학생들의
+-- 이름, 학년, 키 출력
+
+-- 1) 학년별 최대 키
+-- 2) 1)과 같은 값 가진 사람의 이름, 학년, 키
+
+SELECT name, grade, height
+FROM student
+WHERE (grade, height) 
+    IN(
+        SELECT grade, max(height)
+        FROM student
+        GROUP BY grade
+    );
+
+    
